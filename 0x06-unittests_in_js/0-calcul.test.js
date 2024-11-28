2375623495361ff2ca2e6ac87214d05ec4f7e57f
laribox <laribox2014@gmail.com>
2024-11-28 21:26:43 +0000

    tasks

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,35 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-    it('should return the sum of rounded inputs', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1.4, 4.5), 6);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+describe("Tests calculateNumber function:", function() {
+    describe("Calculates two integers:", function() {
+        it('Returns 4.', function() {
+            assert.strictEqual(calculateNumber(1, 3), 4);
+        });
+    });
+    describe("Calculates one float & one integer:", function() {
+        it('Returns 5.', function() {
+            assert.strictEqual(calculateNumber(1, 3.7), 5);
+        });
+    });
+    describe("Calculates one float & one integer (round down):", function() {
+        it('Returns 4.', function() {
+            assert.strictEqual(calculateNumber(1, 3.3), 4);
+        });
+    });
+    describe("Calculates one float & one int reversed:", function () {
+    it('Returns 5.', function () {
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+    });
+  });
+    describe("Calculates two floats:", function() {
+        it('Returns 5.', function() {
+            assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        });
+    });
+    describe("Calculates two floats w/borderline:", function() {
+        it('Returns 6.', function() {
+            assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+        });
     });
 });
-
